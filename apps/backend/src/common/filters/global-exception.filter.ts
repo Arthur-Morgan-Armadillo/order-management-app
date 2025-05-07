0fd5511402c827acb1f28907a98d9c64321f00f0
Arthur <armadillo.morgan@hotmail.com>
2025-05-07 13:09:02 +0300

    Create global exception filter and barrel file

---
+++ apps/backend/src/common/filters/global-exception.filter.ts
@@ -0,0 +1,89 @@
+import {
+  ExceptionFilter,
+  Catch,
+  Logger,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+import { IServerErrorResponse } from '@shared/interfaces';
+
+type ValidationErrorMessage = { message: string | string[] };
+
+@Catch()
+export class GlobalExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(GlobalExceptionFilter.name, {
+    timestamp: true,
+  });
+
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  catch(exception: unknown, host: ArgumentsHost): void {
+    const { httpAdapter } = this.httpAdapterHost;
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    let httpStatus: number = HttpStatus.INTERNAL_SERVER_ERROR;
+    let errorMessage: string = 'Something went wrong';
+    let errorName: string = 'InternalServerException';
+
+    if (exception instanceof HttpException) {
+      httpStatus = exception.getStatus();
+      errorMessage = exception.message;
+      errorName = exception.name;
+
+      if (exception instanceof BadRequestException) {
+        const responseBody = exception.getResponse();
+
+        if (
+          typeof responseBody === 'object' &&
+          responseBody !== null &&
+          (responseBody as ValidationErrorMessage).message
+        ) {
+          const message = (responseBody as ValidationErrorMessage).message;
+          if (Array.isArray(message)) {
+            errorMessage = message.join('; ');
+          } else if (typeof message === 'string') {
+            errorMessage = message;
+          } else {
+            errorMessage = 'Invalid input data';
+          }
+        }
+      }
+
+      if (
+        !(
+          exception instanceof NotFoundException ||
+          exception instanceof BadRequestException ||
+          httpStatus === HttpStatus.TOO_MANY_REQUESTS
+        )
+      ) {
+        this.logger.warn(
+          `Unexpected HTTP Exception: ${errorName} - ${errorMessage} - ${request.method} ${request.url}`,
+        );
+        httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        errorMessage = 'Internal server error';
+        errorName = 'InternalServerError';
+      }
+    } else {
+      this.logger.error(
+        `Non-HTTP Exception: ${exception} - ${request.method} ${request.url}`,
+      );
+    }
+
+    const responseBody: IServerErrorResponse = {
+      status: 'error',
+      statusCode: httpStatus,
+      message: errorMessage,
+      error: errorName,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    };
+
+    httpAdapter.reply(response, responseBody, httpStatus);
+  }
+}
