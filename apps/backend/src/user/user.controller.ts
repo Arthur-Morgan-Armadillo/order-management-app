bb4f50ca7c5b12ad6865218e24f3a3217c020724
Arthur <armadillo.morgan@hotmail.com>
2025-05-07 14:21:55 +0300

    Create user controller file. Implement /GET /users endpoint

---
+++ apps/backend/src/user/user.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, Logger } from '@nestjs/common';
+import { UserService } from './user.service';
+import { IUser, IServerSuccessResponse } from '@shared/interfaces';
+
+@Controller('users')
+export class UserController {
+  private readonly logger = new Logger(UserController.name, {
+    timestamp: true,
+  });
+
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async getAllUsers(): Promise<IServerSuccessResponse<IUser[]>> {
+    this.logger.log('Getting all users');
+    const users = await this.userService.getAllUsers();
+
+    this.logger.log(`Returning ${users.length} user(s)`);
+    return {
+      status: 'success',
+      payload: {
+        data: users,
+      },
+    };
+  }
+}
