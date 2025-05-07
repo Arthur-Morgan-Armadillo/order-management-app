03cd47764e6f238cb9da77f449dd154ae2f1f621
Arthur <armadillo.morgan@hotmail.com>
2025-05-07 14:16:15 +0300

    Create product service file. Implement get all products method

---
+++ apps/backend/src/product/product.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { ProductRepository } from './product.repository';
+import { IProduct } from '@shared/interfaces';
+
+@Injectable()
+export class ProductService {
+  private readonly logger = new Logger(ProductService.name, {
+    timestamp: true,
+  });
+
+  constructor(private readonly productRepository: ProductRepository) {}
+
+  async getAllProducts(): Promise<IProduct[]> {
+    this.logger.log('Getting all products');
+
+    try {
+      const products = await this.productRepository.findAllProducts();
+      if (products.length === 0) {
+        this.logger.warn('No products found');
+        throw new NotFoundException('No products found');
+      }
+
+      const productsReturn: IProduct[] = products.map((product) => ({
+        id: product.id,
+        name: product.name,
+        price: product.price.toFixed(2),
+        stock: product.stock,
+      }));
+
+      this.logger.log(`Returning ${productsReturn.length} product(s)`);
+      return productsReturn;
+    } catch (err) {
+      this.logger.error('Error getting all products', err);
+      throw err;
+    }
+  }
+}
