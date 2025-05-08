080df3bd561d218e251e70c183af8894a2baf54c
Arthur <armadillo.morgan@hotmail.com>
2025-05-08 13:12:37 +0300

    Refactor error file

--- apps/frontend/src/app/error.tsx
+++ apps/frontend/src/app/error.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { JSX } from 'react';
+import React from 'react';
 import { Button } from '@/components';
 
 interface IErrorProps {
@@ -8,7 +8,7 @@ interface IErrorProps {
   reset: () => void;
 }
 
-const Error = ({ reset }: IErrorProps): JSX.Element => {
+const Error = ({ reset }: IErrorProps): React.JSX.Element => {
   return (
     <div className='flex flex-col items-center justify-center py-20 text-center px-4'>
       <h2 className='text-3xl font-bold mb-4'>Something went wrong</h2>
